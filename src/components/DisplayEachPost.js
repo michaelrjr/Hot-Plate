58243ef4c7261db96a660e1f22da63afba262b45
Lorenzo Diago <lorenzodiago2@outlook.com>
2021-04-12 20:03:25 +0100

    Like button being developed into effect

--- src/components/DisplayEachPost.js
+++ src/components/DisplayEachPost.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { FaRegCommentDots } from "react-icons/fa";
 import { AiOutlineLike } from "react-icons/ai";
 import CommentBox from "./CommentBox";
+import { firebase } from "@firebase/app";
 import { Link } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 import ComponentC from "./ComponentC";
@@ -11,15 +12,46 @@ import app from "../firebase";
 
 export default function DisplayEachPost(props) {
   const [showCommentBox, setShowCommentBox] = useState(false);
-  const { CheckCommentsExist } = useAuth();
+  const { CheckCommentsExist, currentUser } = useAuth();
   const userDBRef = app.firestore().collection("Users");
+  const likeRef = app.firestore().collection("feed").doc(props.postID).collection("Likes");;
+  
   const [ currentUserData, setCurrentUserData ] = useState();
+
+  const [likeOrUnlike, setLikeOrUnlike] = useState(false);
+
+  useEffect(() => {
+    checkLiked();
+  }, []);
+
+  const checkLiked = () => {
+    likeRef.doc(currentUser.email).get().then((docSnapshot) => {
+      if (!docSnapshot.exists) setLikeOrUnlike(true);
+    })
+  }
 
   const showCommentInputBox = () => {
     setShowCommentBox(!showCommentBox);
   };
-
   
+  const likedPost = () => {
+    likeRef.doc(currentUser.email).get().then((docSnapshot) => {
+      if (!docSnapshot.exists) {
+        likeRef.doc(currentUser.email)
+          .set({
+            email: currentUser.email,
+            uid: currentUser.uid,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+          });
+          setLikeOrUnlike(true);
+      }else{
+        likeRef.doc(currentUser.email)
+          .delete();
+        setLikeOrUnlike(false);
+      }
+    })
+  }
+
   const getUserData = () => {
     userDBRef.doc(props.email).get().then( (doc) => {
       setCurrentUserData( doc.data() );
@@ -60,11 +92,12 @@ export default function DisplayEachPost(props) {
               />
             }
             <div className="d-inline">
-              <button className="btn btn-like btn-sm w-50 d-inline">
+              <button className="btn btn-like btn-sm w-50 d-inline" onClick={()=> likedPost()}>
                 <div className="d-inline mr-1">
                   <AiOutlineLike />
                 </div>
-                <div className="d-inline">Like</div>
+                {!likeOrUnlike && <div className="d-inline">Like</div>}
+                {likeOrUnlike && <div className="d-inline">Unlike</div>}
               </button>
             </div>
             <div className="d-inline">
