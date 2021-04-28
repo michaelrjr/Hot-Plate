d65ecf9176280ebe2990d647286891bac4874c31
JPFreely <jpshoulihan@gmail.com>
2021-04-28 23:35:56 +0100

    directory clean

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,12 @@ import Feed from "./Feed";
 import NavBar from "./NavBar";
 import Profile from "./Profile";
 import RecipeSearch from "./RecipeSearch";
-import Chat from "./Chat";
+import Chat from "./Chat/Chat";
 import CreateRecipe from "./CreateRecipe";
 import MyRecipes from "./MyRecipes";
 import MyRecipes2 from "./MyRecipes2";
+
+//style={{ maxWidth: "450px" }} for main div container
 
 function App() {
   return (
