fe9fb3401d16fbbd48a58454e56f61631b31fb3f
JPFreely <jpshoulihan@gmail.com>
2021-05-10 22:26:19 +0100

    feed clean logs imports

--- src/components/Feed/CommentBox.js
+++ src/components/Feed/CommentBox.js
@@ -39,11 +39,8 @@ export default function CommentBox(props) {
                 lastName: userData.lastName,
                 timestamp: firebase.firestore.FieldValue.serverTimestamp(),
               })
-              // .then((docRef) => {
-              //   // console.log("Document written with ID: ", docRef.id);
-              // })
               .catch((error) => {
-                console.error("Error adding document: ", error);
+                throw error;
               });
           }
         });
